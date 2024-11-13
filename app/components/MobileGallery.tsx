5286bbe55241c801f94c58dee60cbbff37ca8681
Arturo Baylon <abaylon@tiprotec.com>
2024-11-12 18:11:30 -0600

    cambios 2

--- app/components/MobileGallery.tsx
+++ app/components/MobileGallery.tsx
@@ -2,7 +2,6 @@ import { projectList } from "../assets/information";
 import Image from "next/image";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
-import { section } from "framer-motion/client";
 
 export const MobileGallery = () => {
   return (
@@ -11,49 +10,52 @@ export const MobileGallery = () => {
         FILMAKER
         <span className="text-xl font-normal">@Edgar Baylon</span>
       </h1>
-      {projectList.map((item, index) => {
-        const ref = useRef(null);
-        const isInView = useInView(ref);
-
-        const isEven = index % 2 === 0;
-        return (
-          <motion.div
-            ref={ref}
-            initial={{ x: isEven ? -100 : 100, opacity: 0 }}
-            animate={isInView ? { x: 0, opacity: 1 } : {}}
-            transition={{ duration: 1 }}
-            key={index}
-            className={`flex z-10 relative flex-col ${
-              isEven ? "items-start pl-4" : "items-end pr-4"
-            } w-full  lg:hidden p-10`}
-          >
-            <a href={`/project/${index}`}>
-              <Image
-                className=" w-[350px] h-[300px] rounded-sm"
-                src={item.image}
-                alt={item.title}
-                width={500}
-                height={500}
-              />
-              <h2
-                className={`mt-2 text-3xl font-bold   ${
-                  isEven ? "text-left " : "text-right "
-                }`}
-              >
-                <span className="text-sm ">0{index + 1}</span>{" "}
-                {item.title.toUpperCase()}
-              </h2>
-              <p
-                className={`mt-1 w-80  font-thin ${
-                  isEven ? "text-left" : "text-right "
-                } `}
-              >
-                {item.description}
-              </p>
-            </a>
-          </motion.div>
-        );
-      })}
+      {projectList.map((item, index) => (
+        <GalleryItem key={index} item={item} index={index} />
+      ))}
     </section>
+  );
+};
+
+const GalleryItem = ({ item, index }: { item: any; index: any }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const isEven = index % 2 === 0;
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ x: isEven ? -100 : 100, opacity: 0 }}
+      animate={isInView ? { x: 0, opacity: 1 } : {}}
+      transition={{ duration: 1 }}
+      className={`flex z-10 relative flex-col ${
+        isEven ? "items-start pl-4" : "items-end pr-4"
+      } w-full lg:hidden p-10`}
+    >
+      <a href={`/project/${index}`}>
+        <Image
+          className="w-[350px] h-[300px] rounded-sm"
+          src={item.image}
+          alt={item.title}
+          width={500}
+          height={500}
+        />
+        <h2
+          className={`mt-2 text-3xl font-bold ${
+            isEven ? "text-left" : "text-right"
+          }`}
+        >
+          <span className="text-sm">0{index + 1}</span>{" "}
+          {item.title.toUpperCase()}
+        </h2>
+        <p
+          className={`mt-1 w-80 font-thin ${
+            isEven ? "text-left" : "text-right"
+          }`}
+        >
+          {item.description}
+        </p>
+      </a>
+    </motion.div>
   );
 };
